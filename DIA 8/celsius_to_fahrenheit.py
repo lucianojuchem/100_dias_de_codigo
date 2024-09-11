913cde9c63e0bae3a75b677ab87600e816665710
lucianojuchem <lucianojuchem@gmail.com>
2024-09-11 08:30:57 -0300

    DIA 8

---
+++ DIA 8/celsius_to_fahrenheit.py
@@ -0,0 +1,9 @@
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+
+celsius = float(input("Digite a temperatura em Celsius: "))
+
+fahrenheit = celsius_to_fahrenheit(celsius)
+print(f"{celsius}°C é igual a {fahrenheit}°F")
